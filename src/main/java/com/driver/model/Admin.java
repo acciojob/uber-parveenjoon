66793c398d7bd0225a90373c01a1c0ac08558868
parveenjoon <47267545+parveenjoon@users.noreply.github.com>
2024-04-03 19:56:14 +0530

    Update Admin.java

---
+++ src/main/java/com/driver/model/Admin.java
@@ -0,0 +1,48 @@
+
+package com.driver.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "admins")
+public class Admin {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer adminId;
+
+    String username;
+
+    String password;
+
+    public Admin() {
+    }
+
+    public Admin(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public Integer getAdminId() {
+        return adminId;
+    }
+
+    public void setAdminId(Integer adminId) {
+        this.adminId = adminId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
